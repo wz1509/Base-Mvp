3033cceced5996681f98c198f91dd278bbffc83b
wz1509 <727417703@qq.com>
2018-08-27 18:32:34 +0800

    优化dagger2使用方式

---
+++ app/src/main/java/com/wazing/mvp/di/component/AppComponent.kt
@@ -0,0 +1,25 @@
+package com.wazing.mvp.di.component
+
+import com.wazing.mvp.application.AppApplication
+import com.wazing.mvp.di.module.ActivityModule
+import com.wazing.mvp.di.module.NetModule
+import com.wazing.mvp.model.network.ApiService
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.support.AndroidSupportInjectionModule
+import retrofit2.Retrofit
+
+@Component(modules = [
+    AndroidInjectionModule::class,
+    AndroidSupportInjectionModule::class,
+    ActivityModule::class,
+    NetModule::class
+])
+interface AppComponent {
+
+    fun inject(application: AppApplication)
+
+    fun getRetrofit(): Retrofit
+
+    fun getApiService(): ApiService
+}
